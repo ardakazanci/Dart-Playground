171a6d36fb63ca7ccfd87e6d2a04973f6d97deb6
ardakazanci <ardakazanci@anadolu.edu.tr>
2020-04-04 17:06:16 +0300

    # Functions Added Exercise - 2

--- functions_10/Main.dart
+++ functions_10/Main.dart
@@ -5,8 +5,11 @@
 
 void main() {
   print(functionsAnothmy("Arda"));
+  print(arrowFunctions("Arda"));
 }
 
 String functionsAnothmy(String value) {
   return value;
 }
+
+String arrowFunctions(String value) => "Hello ${value}";
