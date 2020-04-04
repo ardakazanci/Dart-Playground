6404567be884ace431b5aad1f8197829895a3938
ardakazanci <ardakazanci@anadolu.edu.tr>
2020-04-04 18:16:23 +0300

    # Numbers Type Added Exercise

---
+++ numbers_type_12/Main.dart
@@ -0,0 +1,15 @@
+void main() {
+  /*
+
+    int  = 0
+    double = 0.0
+    sum = int + double
+
+   */
+
+  int intSample = 0;
+  double doubleSample = 0.0;
+  double doubleSampleSecond = 0;
+  num numSampleOne = 0.0;
+  num numSampleSecond = 0;
+}
