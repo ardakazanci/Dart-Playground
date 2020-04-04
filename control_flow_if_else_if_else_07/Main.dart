8db181109661cd1d1df922f93b4d17c5361d621d
ardakazanci <ardakazanci@anadolu.edu.tr>
2020-04-04 16:54:23 +0300

    # Control Flows  if-else if - else Added Exercise

---
+++ control_flow_if_else_if_else_07/Main.dart
@@ -0,0 +1,13 @@
+void main() {
+  var sample = true;
+
+  if (sample) {
+    print("Sample is ${sample}");
+  } else if (!sample) {
+    print("Sample is false");
+  } else {
+    print("Sample is nothing :(");
+  }
+
+  // bla bla bla ...
+}
