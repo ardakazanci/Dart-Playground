a7a1c857de5929f905fa98bbc9c93ba9eeeac678
ardakazanci <ardakazanci@anadolu.edu.tr>
2020-04-04 17:04:53 +0300

    # Functions Added Exercise -1

---
+++ functions_10/Main.dart
@@ -0,0 +1,12 @@
+/*
+  ReturnType functionName(ArgumentType arg)
+  or void returned
+ */
+
+void main() {
+  print(functionsAnothmy("Arda"));
+}
+
+String functionsAnothmy(String value) {
+  return value;
+}
