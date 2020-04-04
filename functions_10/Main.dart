75f6d00970e9371b9c3fcf3617602d77e74bda3e
ardakazanci <ardakazanci@anadolu.edu.tr>
2020-04-04 17:11:59 +0300

    # Functions Added Exercise - 3

--- functions_10/Main.dart
+++ functions_10/Main.dart
@@ -6,6 +6,9 @@
 void main() {
   print(functionsAnothmy("Arda"));
   print(arrowFunctions("Arda"));
+  debugger(arg1: "Arda", arg2: "Kazancı");
+  debuggerSecond(1, 2, 3);
+  debuggerSecond(1, 2);
 }
 
 String functionsAnothmy(String value) {
@@ -13,3 +16,21 @@ String functionsAnothmy(String value) {
 }
 
 String arrowFunctions(String value) => "Hello ${value}";
+
+void debugger({String arg1, String arg2}) {}
+
+/*
+  Sample in flutter
+
+    import 'package:meta/meta.dart'
+
+    Widget build({@required Widget child}) {
+      //...
+    }
+ */
+
+// Opsional
+void debuggerSecond(int arg1, int arg2, [int arg3]) {}
+
+// Default Value
+int debuggerThird(int x, int y, [int z = 25]) => x + y + z;
